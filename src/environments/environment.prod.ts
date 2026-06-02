8a84f908818404c35fd45be82a69d9cf0cb1f5e9
KVergasta <kauvergasta12@gmail.com>
2026-06-02 16:59:40 -0300

    deploy: publicando a interface final em Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://notificationhubapi.onrender.com' // Endereço gerado pela Render
+  apiUrl: 'https://notificationhubapi.onrender.com' , // Endereço gerado pela Render
+  firebaseConfig: {
+    apiKey: "AIzaSyC-IGwzIqCC2mpXJxxCCjhw4eVkzNdIXog",
+    vapidKey: 'BK_RHeMFHZJ00BzzWD-1EZvYhwulOcmCZI2Uo-yA98UwPLcdvlnY176afvNfRcG_5fXrpHPs7ma3G_l5HcDH7Wg',
+    authDomain: "notificationhub-d9fb5.firebaseapp.com",
+    projectId: "notificationhub-d9fb5",
+    storageBucket: "notificationhub-d9fb5.firebasestorage.app",
+    messagingSenderId: "791895927795",
+    appId: "1:791895927795:web:0f76812b85b5d9709fc086",
+    measurementId: "G-V1Y7QH4S3L"
+  }
 };
